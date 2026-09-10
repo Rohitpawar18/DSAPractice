2b98f3cadb757cee5982297ae7dcd262c0e4e960
Rohit Pawar <rohitshrikantpawar@gmail.com>
2026-09-10 14:50:37 +0530

    Sort Colors leetcode probelem solved

---
+++ Array/SortColors.java
@@ -0,0 +1,30 @@
+public class SortColors {
+  public static void main(String[] args) {
+    int[] nums = {2,0,2,1,1,0};
+    sortColors(nums);
+
+    System.out.print("[");
+    for(int i = 0; i < nums.length; i++){
+      System.out.print(nums[i] + "" + (i < nums.length - 1 ? "," : ""));
+    }
+    System.out.println("]");
+  }
+
+  public static void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+                int tmp = nums[low];
+                nums[low++] = nums[mid];
+                nums[mid++] = tmp;
+            }else if (nums[mid] == 1){
+                mid++;
+            }else{
+                int tmp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high--] = tmp;
+            }
+        }
+  }
+}
